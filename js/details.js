9aa6972f48cd28a9c941a2d081eaaec603927e28
TaniaRobledo <tania.robledoabenojar@riberadeltajo.es>
2024-12-27 10:51:34 +0100

    Obtener nombre desde el localStorage del videojuego

--- js/details.js
+++ js/details.js
@@ -7,6 +7,7 @@ const getLsItem = () => {
 // Obtener los datos del videojuego
 const videoGameDetails = getLsItem();
 
+// Obtener la imagen
 if (videoGameDetails && videoGameDetails.background_image) {
   // Seleccionar el contenedor
   const gameImageContainer = document.getElementById('game_image');
@@ -24,4 +25,30 @@ if (videoGameDetails && videoGameDetails.background_image) {
   gameImageContainer.style.backgroundSize = 'cover';
   gameImageContainer.style.backgroundPosition = 'center';
   gameImageContainer.style.backgroundRepeat = 'no-repeat';
+}
+
+// Obtener el nombre del juego
+if(videoGameDetails && videoGameDetails.name){
+  const gameName = document.getElementById('game_name');
+  gameName.textContent = videoGameDetails.name;
+}
+
+// Seleccionar el contenedor donde se agregarán las imágenes
+const screenshotsContainer = document.getElementById('screenshots-container');
+
+// Verificar si el objeto tiene imágenes
+if (videoGameDetails && videoGameDetails.short_screenshots) {
+  // Recorrer las imágenes y generar contenedores dinámicamente
+  videoGameDetails.short_screenshots.forEach((screenshot) => {
+    // Crear un elemento img para cada screenshot
+    const imgElement = document.createElement('img');
+    imgElement.src = screenshot.image;
+    imgElement.classList.add('h-32', 'object-cover', 'rounded', 'shadow', 'mb-8'); 
+    
+    // Agregar la imagen al contenedor
+    screenshotsContainer.appendChild(imgElement);
+  });
+} else {
+  // Mostrar un mensaje si no hay screenshots disponibles
+  screenshotsContainer.innerHTML = '<p class="text-gray-500">No hay imágenes disponibles para este juego.</p>';
 }
